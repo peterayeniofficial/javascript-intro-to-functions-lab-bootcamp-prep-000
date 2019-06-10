f1b15a57a75ad86df0cdaa36d06999a2ce9248d4
Peter Ayeni <ayenicology@gmail.com>
2019-06-10 21:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper() {
 
 function sayHiToGranma(string) {
   if (string.toUpperCase() === string) {
-    
+    return "YES INDEED!"
   }
   if (string.toLowerCase() === string){
-    
+    return "I can't hear you!"
   }
 }
